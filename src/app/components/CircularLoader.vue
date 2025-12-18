93edb90d4cd7966843459ac4985d8f1f19ee053d
colorful-jelly-beans <mark.izraylev.dev@gmail.com>
2025-12-18 18:46:05 +0300

    Add circular loader

---
+++ src/app/components/CircularLoader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="loader"></div>
+</template>
+
+<style scoped>
+.loader {
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  border: 1px solid transparent;
+  border-top: 1px solid var(--color-background-loader-primary);
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
