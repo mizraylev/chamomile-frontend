fc01fda3bf3246f51f31f2d2456b6b4cdd6c952a
colorful-jelly-beans <mark.izraylev.dev@gmail.com>
2025-12-20 14:47:04 +0300

    Add API client with an interceptor

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,1 +1,21 @@
-export const API_BASE_URL = 'http://localhost:4000'
+import axios from 'axios'
+import router from '../router'
+
+const API_BASE_URL = `http://${window.location.hostname}:4000`
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true,
+})
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      router.push('/login')
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default apiClient
