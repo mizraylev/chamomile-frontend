8024bc489146018175f35fcfc8b881b0ac020888
colorful-jelly-beans <mark.izraylev.dev@gmail.com>
2025-12-20 15:33:58 +0300

    Add chat type

---
+++ src/chatList/utils/types.ts
@@ -0,0 +1,9 @@
+export type Chat = {
+  id: string
+  name: string
+  creationTime: string
+  companionNickname: string
+  companionLastSeen: string
+  lastMessageContent: string
+  lastMessageTimestamp: string
+}
